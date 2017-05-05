1fc436258078a0fe1710bb1f07387fbd110afb5b
Jose Luis Vaca Fernandez <vakajose@gmail.com>
2017-05-05 01:13:36 -0400

    Update Main.java

--- MyFirstJavaProject/src/main/Main.java
+++ MyFirstJavaProject/src/main/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("this is a secondary branch");
+		System.out.println("this is a change from github.com");
 	}
 
 }
